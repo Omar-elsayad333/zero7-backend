02b9cb00a5a378ec39ff563e3e4e286b581150f2
Omar-elsayad333 <omarelsayad333@gmail.com>
2023-11-24 20:42:49 +0200

    develop

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,76 +1,73 @@
-const { check, validationResult } = require("express-validator");
+const { check, validationResult } = require('express-validator')
 
 const userSignUpValidationRules = () => {
   return [
-    check("name", "Name is required").not().isEmpty(),
-    check("email", "Invalid email").not().isEmpty().isEmail(),
-    check("password", "Please enter a password with 4 or more characters")
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Invalid email').not().isEmpty().isEmail(),
+    check('password', 'Please enter a password with 4 or more characters')
       .not()
       .isEmpty()
       .isLength({ min: 4 }),
-  ];
-};
+  ]
+}
 
 const userSignInValidationRules = () => {
   return [
-    check("email", "Invalid email").not().isEmpty().isEmail(),
-    check("password", "Invalid password").not().isEmpty().isLength({ min: 4 }),
-  ];
-};
+    check('email', 'Invalid email').not().isEmpty().isEmail(),
+    check('password', 'Invalid password').not().isEmpty().isLength({ min: 4 }),
+  ]
+}
 
 const userContactUsValidationRules = () => {
   return [
-    check("name", "Please enter a name").not().isEmpty(),
-    check("email", "Please enter a valid email address")
-      .not()
-      .isEmpty()
-      .isEmail(),
-    check("message", "Please enter a message with at least 10 words")
+    check('name', 'Please enter a name').not().isEmpty(),
+    check('email', 'Please enter a valid email address').not().isEmpty().isEmail(),
+    check('message', 'Please enter a message with at least 10 words')
       .not()
       .isEmpty()
       .isLength({ min: 10 }),
-  ];
-};
+  ]
+}
 
 const validateSignup = (req, res, next) => {
-  const errors = validationResult(req);
+  const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    var messages = [];
+    var messages = []
     errors.array().forEach((error) => {
-      messages.push(error.msg);
-    });
-    req.flash("error", messages);
-    return res.redirect("/user/signup");
+      messages.push(error.msg)
+    })
+    req.flash('error', messages)
+    return res.redirect('/user/signup')
   }
-  next();
-};
+  next()
+}
 
 const validateSignin = (req, res, next) => {
-  const errors = validationResult(req);
+  const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    var messages = [];
+    var messages = []
     errors.array().forEach((error) => {
-      messages.push(error.msg);
-    });
-    req.flash("error", messages);
-    return res.redirect("/user/signin");
+      messages.push(error.msg)
+    })
+    req.flash('error', messages)
+    return res.redirect('/user/signin')
   }
-  next();
-};
+  next()
+}
 
 const validateContactUs = (req, res, next) => {
-  const errors = validationResult(req);
+  const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    var messages = [];
+    var messages = []
     errors.array().forEach((error) => {
-      messages.push(error.msg);
-    });
-    console.log(messages);
-    req.flash("error", messages);
-    return res.redirect("/pages/contact-us");
+      messages.push(error.msg)
+    })
+    console.log(messages)
+    req.flash('error', messages)
+    return res.redirect('/pages/contact-us')
   }
-  next();
-};
+  next()
+}
 
 module.exports = {
   userSignUpValidationRules,
@@ -79,4 +76,4 @@ module.exports = {
   validateSignup,
   validateSignin,
   validateContactUs,
-};
+}
